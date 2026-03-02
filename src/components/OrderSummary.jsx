a662adbdf04ecb5b0b2f58934d5a82b38016b33e
DavidPopovich3261 <8534397@gmail.com>
2026-03-02 13:30:13 +0200

    123

--- src/components/OrderSummary.jsx
+++ src/components/OrderSummary.jsx
@@ -5,24 +5,12 @@ function OrderSummary() {
     const carts = useStore((s) => s.carts)
     const count = useStore((s) => s.count)
     const TotalPrice = useStore((s) => s.TotalPrice)
-    let contPrice = 0
-    // const addPrice = useStore((s) => s.addPrice)
-    // useEffect(() => {
-    //     for (let item of carts) {
-    //         console.log(item.price);
-    //         contPrice += item.price
-    //     }
-    //     console.log(contPrice);
-    //     incPrice(contPrice)
-    // }, [])
-
-
     return (
         <div>
-            {carts &&
+            {carts[0] &&
                 <div>
                     <p>Total items: {count}</p>
-                    <p>Total price: {TotalPrice}</p>
+                    <p>Total price: { TotalPrice.toFixed(2)}</p>
                 </div>}
         </div>
 
